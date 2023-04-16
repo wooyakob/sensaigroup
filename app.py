b40955c9ba5741090c47297c4369d018f57631e4
Jacob Wood <57792656+wooyakob@users.noreply.github.com>
2023-04-15 18:21:14 -0700

    updating port

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ def chatbot():
     return jsonify(response_text)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
