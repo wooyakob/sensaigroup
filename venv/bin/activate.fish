7f9d774590218bff01cd5d2e7283a91b5c2289eb
Jacob Wood <jake_wood@mac.com>
2023-05-01 14:49:11 -0700

    updating pip, installing requiremnts file

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/yakobwood/Desktop/iObject/venv"
+set -gx VIRTUAL_ENV "/Users/yakobwood/Desktop/Sales Sensei/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
