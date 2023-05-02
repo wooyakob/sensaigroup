72eb3cca983cdffe76614f875b1bc6b2df03331e
Jacob Wood <jake_wood@mac.com>
2023-05-01 18:43:27 -0700

    debug flase, flask sessions

--- app.py
+++ app.py
@@ -105,4 +105,4 @@ def chatbot():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=False)
