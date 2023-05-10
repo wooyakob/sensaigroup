0fb3ff3ed58930edb355fedb0bd7dcb58292b31b
Jacob Wood <jake_wood@mac.com>
2023-05-10 15:47:19 -0700

    UPDATE PORT

--- app.py
+++ app.py
@@ -159,4 +159,4 @@ with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 80)))
