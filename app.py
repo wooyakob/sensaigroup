50d65f90ef83eec01b87b2ab11bde5a2f4a09f7d
Jacob Wood <jake_wood@mac.com>
2023-05-10 17:03:08 -0700

    Revert

--- app.py
+++ app.py
@@ -1,51 +1,43 @@
-# Import python regular expression module, which allows for pattern matching and text manipulation
+#imports
 import re
-# Modules from the Flask web framework, which provides tools and utilities for building web applications in Python
 from flask import Flask, render_template, request, jsonify, redirect, url_for, flash, session
-# Operating system module, which provides a way to interact with the operating system
 import os
-# OpenAI API module, which provides a way to interact with the OpenAI API
 import openai
-# Module for loading environment variables from a .env file
 from dotenv import load_dotenv
-# Module for interacting with a Postgres database
 from flask_sqlalchemy import SQLAlchemy
-# Module for user authentication
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
-# Module for password hashing
+from flask_session import Session
 from argon2 import PasswordHasher
-# Module for password verification
 from argon2.exceptions import VerifyMismatchError
 
-# Load environment variables from .env file
 load_dotenv()
-# Create Flask app
 app = Flask(__name__)
-# Configure Flask app
 ph = PasswordHasher()
-# Configure OpenAI API
 openai.api_key = os.getenv("OPENAI_API_KEY")
-# Configure Postgres Database
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_FILE_DIR'] = './flask_session/'
+Session(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///login.db'
+app.config['SECRET_KEY'] = 'your_secret_key_here'
 db = SQLAlchemy(app)
+
 
 #classes for info that's stored in SQL database
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
-    role = db.Column(db.String(50), nullable=False, default="user")
-    first_name = db.Column(db.String(50), nullable=False)
-    last_name = db.Column(db.String(50), nullable=False)
-    company = db.Column(db.String(100), nullable=False)
 
-# Configure Login Manager
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+@app.route('/landing')
+def landing():
+    return render_template('landing.html')
 
 # Authentication Route, if authenticated display index, if not display landing
 @app.route('/')
@@ -89,6 +81,7 @@ def home():
     return render_template('landing.html')
 
 #Email Validation
+
 def is_valid_email(email):
     regex = r'^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
     return re.match(regex, email)
@@ -102,45 +95,58 @@ def signup():
     if request.method == 'POST':
         email = request.form['email'].strip()
         password = request.form['password'].strip()
-        first_name = request.form['first_name'].strip()
-        last_name = request.form['last_name'].strip()
-        company = request.form['company'].strip()
 
-        if not all([email, password, first_name, last_name, company]):
-            flash('All fields are required.', 'danger')
+        if not email or not password:
+            flash('Email and password cannot be empty.', 'danger')
             return render_template('signup.html')
 
-        ph = PasswordHasher()
+        if not is_valid_email(email):
+            flash('Invalid email format.', 'danger')
+            return render_template('signup.html')
+
+        existing_user = User.query.filter_by(email=email).first()
+        if existing_user:
+            flash('Email already exists. Please choose a different one.', 'danger')
+            return render_template('signup.html')
+
         hashed_password = ph.hash(password)
-        user = User(email=email, password=hashed_password, first_name=first_name, last_name=last_name, company=company)
+        user = User(email=email, password=hashed_password)
         db.session.add(user)
         db.session.commit()
 
-        flash('Thank you for signing up. Please log in to continue your Sales Sensei journey!')
         return redirect(url_for('login'))
     return render_template('signup.html')
 
+@app.after_request
+def add_header(response):
+    response.cache_control.no_store = True
+    response.cache_control.no_cache = True
+    response.cache_control.must_revalidate = True
+    response.cache_control.proxy_revalidate = True
+    response.expires = 0
+    response.pragma = 'no-cache'
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0'
+    response.headers['Expires'] = '-1'
+    return response
 
-# Objection Training
 objections = {
     "We're using your competitor": "And how are you finding them? If you don’t mind me asking, why did you choose to go with them?",
     "Your product is too expensive": "Cost is an important consideration but I believe we can actually save you money. Can we set up a time for me to explain how?",
     "I don't see any ROI potential": "There’s definitely potential. I’d love to show you and explain how. Are you available this week for a more detailed call?",
 }
 
-# Chatbot Route
 @app.route('/chatbot', methods=['POST'])
 def chatbot():
+    questions_asked = session.get('questions_asked', 0)
+    unlimited = session.get('unlimited', False)
     user_input = request.json.get('user_input')
 
-# Manage User Objection Input
     if len(user_input) > 140:
         return jsonify("Objection is too long. Please limit your objection to 140 characters, or less.")
 
-# Prompt Engineering
     prompt = f"A prospective client mentioned, \"{user_input}\" How would you address this concern?\n\nSales Sensei:"
     response = openai.Completion.create(
-        model="davinci:ft-personal-2023-04-17-22-02-12", # Fine Tuned Model
+        model="davinci:ft-personal-2023-04-17-22-02-12",
         prompt=prompt,
         temperature=0,
         max_tokens=100,
@@ -149,12 +155,12 @@ def chatbot():
         presence_penalty=2,
         stop=["END",]
     )
-    response_text = response.choices[0].text.strip() # Remove leading and trailing whitespace
-    response_text = re.search(r'(.*[.!?])', response_text).group(0) # Remove trailing text after punctuation
-    return jsonify(response_text) # Return response to user
+    response_text = response.choices[0].text.strip()
+    response_text = re.search(r'(.*[.!?])', response_text).group(0)
+    return jsonify(response_text)
 
-with app.app_context(): # Create all tables in database
-    db.create_all() # Create all tables in database
+with app.app_context():
+    db.create_all()
 
-if __name__ == "__main__": # Run app
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 25060)))
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
