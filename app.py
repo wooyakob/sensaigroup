b5895d3d618f33609930c0ee2ec58abdf5713423
Jacob Wood <jake_wood@mac.com>
2023-05-11 10:23:23 -0700

    update routes

--- app.py
+++ app.py
@@ -1,71 +1,50 @@
-# Import necessary libraries
-# Python Regular Expression Library
+#imports
 import re
-
-# Import Flask libraries and components
 from flask import Flask, render_template, request, jsonify, redirect, url_for, flash, session
-
-# Import other required libraries
 import os
 import openai
-
-# Import environment variable loader
 from dotenv import load_dotenv
-# Import SQLAlchemy for database management
 from flask_sqlalchemy import SQLAlchemy
-# Import Flask-Login for user authentication
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
-# Import Flask-Session for session management
 from flask_session import Session
-# Import Argon2 for password hashing
 from argon2 import PasswordHasher
 from argon2.exceptions import VerifyMismatchError
 
-# Load environment variables from .env file
 load_dotenv()
 app = Flask(__name__)
 ph = PasswordHasher()
 openai.api_key = os.getenv("OPENAI_API_KEY")
-
-# Configure session storage
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SESSION_FILE_DIR'] = './flask_session/'
 Session(app)
-
-# Configure SQLAlchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///login.db'
 app.config['SECRET_KEY'] = 'your_secret_key_here'
 db = SQLAlchemy(app)
 
-# Define User model for the database
+
+#classes for info that's stored in SQL database
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
 
-# Initialize Flask-Login
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
-
-# Define user_loader for Flask-Login
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-# Landing page route
-@app.route('/landing')
-def landing():
-    return render_template('landing.html')
-
-# Index route with authentication
+# Home Page
 @app.route('/')
 def index():
-    if current_user.is_authenticated:
-        session['unlimited'] = current_user.is_authenticated
-        return render_template('index.html')
-    else:
-        return redirect(url_for('landing'))
+    return render_template('landing.html')
+
+# Application for Authenticated Users
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('index.html')
 
 # Login Route
 @app.route('/login', methods=['GET', 'POST'])
@@ -81,13 +60,13 @@ def login():
             try:
                 if ph.verify(user.password, password):
                     login_user(user)
-                    return redirect(url_for('index'))
+                    return redirect(url_for('dashboard'))
             except VerifyMismatchError:
                 pass
         flash('Invalid email or password.', 'danger')
     return render_template('login.html')
 
-#Logout Route
+# Logout Route
 @app.route('/logout')
 @login_required
 def logout():
@@ -135,7 +114,6 @@ def signup():
         return redirect(url_for('login'))
     return render_template('signup.html')
 
-# Add headers to response to prevent caching
 @app.after_request
 def add_header(response):
     response.cache_control.no_store = True
@@ -148,28 +126,24 @@ def add_header(response):
     response.headers['Expires'] = '-1'
     return response
 
-# Define objection dictionary
 objections = {
     "We're using your competitor": "And how are you finding them? If you don’t mind me asking, why did you choose to go with them?",
     "Your product is too expensive": "Cost is an important consideration but I believe we can actually save you money. Can we set up a time for me to explain how?",
     "I don't see any ROI potential": "There’s definitely potential. I’d love to show you and explain how. Are you available this week for a more detailed call?",
 }
 
-# Chatbot route
 @app.route('/chatbot', methods=['POST'])
 def chatbot():
     questions_asked = session.get('questions_asked', 0)
     unlimited = session.get('unlimited', False)
     user_input = request.json.get('user_input')
 
-# Restrict objection token length
     if len(user_input) > 140:
         return jsonify("Objection is too long. Please limit your objection to 140 characters, or less.")
 
-# Prompt engineering
     prompt = f"A prospective client mentioned, \"{user_input}\" How would you address this concern?\n\nSales Sensei:"
     response = openai.Completion.create(
-        model="davinci:ft-personal-2023-04-17-22-02-12", # Unique training model ID
+        model="davinci:ft-personal-2023-04-17-22-02-12",
         prompt=prompt,
         temperature=0,
         max_tokens=100,
@@ -182,10 +156,8 @@ def chatbot():
     response_text = re.search(r'(.*[.!?])', response_text).group(0)
     return jsonify(response_text)
 
-# Create database tables with app context
 with app.app_context():
     db.create_all()
 
-# Run the app
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
