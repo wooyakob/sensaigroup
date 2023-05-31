e9ca69274583a0cc984b424e2fdbb37ada46789b
Jacob Wood <jake_wood@mac.com>
2023-05-31 09:01:01 -0700

    fix to signup form, company field

--- app.py
+++ app.py
@@ -161,6 +161,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(100), nullable=False)
     last_name = db.Column(db.String(100), nullable=False)
+    company = db.Column(db.String(120))
     email = db.Column(db.String(100), unique=True, nullable=False)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
