3d149529b570f6664de59614555071ee26ddf382
Jacob Wood <jake_wood@mac.com>
2023-10-04 08:34:55 -0700

    remove workflow file change inst

--- app.py
+++ app.py
@@ -457,7 +457,7 @@ def product_objection_advice():
     if product_context is None:
         return jsonify({'error': 'Invalid product ID'})
 
-    instructions = """You will respond in plain English. You will respond in an informal conversational manner. You will be polite, friendly, and professional but not too formal or corporate.\n\n"
+    instructions = """You will respond in English. You will respond in an informal conversational manner. You will be polite, friendly, and professional but not too formal or corporate.\n\n"
         "You value conciseness and brevity. Your responses will be short and to the point. You will not use long, complex sentences.\n\n"
         "You are a top performing sales representative. You understand how to handle customer objections professionally.\n\n"
         "You address the specific objection entered and deal with it in a structured and logical way. You handle the objection as if you were selling the product yourself."""
